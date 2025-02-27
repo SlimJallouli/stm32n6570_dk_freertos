277833feeb57c77a2a65bcd54f9c5c2ed294ffe9
SlimJallouli <slim.jallouli@st.com>
2025-02-27 12:23:58 -0800

    optimise

--- update.sh
+++ update.sh
@@ -1,223 +1,74 @@
 #!/bin/bash
 
-# Define source and destination directories
 HOME=~
-FreeRTOS_VERSON="11.1.0"
+COMPILER="GCC"
+CORE="ARM_CM55_NTZ"
+FreeRTOS_VERSION="11.1.0"
 
-FreeRTOS_CONFIG_FILE="FreeRTOSConfig.h"
-
-FreeRTOS_SRC_Folder="$HOME/STM32Cube/Repository/Packs/ARM/CMSIS-FreeRTOS/$FreeRTOS_VERSON/Source"
+# Define source and destination directories
+FreeRTOS_SRC_Folder="$HOME/STM32Cube/Repository/Packs/ARM/CMSIS-FreeRTOS/$FreeRTOS_VERSION/Source"
 FreeRTOS_DST_Folder="./Middlewares/Third_Party/ARM_RTOS_FreeRTOS"
 
 # Create the destination directory if it doesn't exist
-if [ ! -d "$FreeRTOS_DST_Folder" ]; then
-    mkdir -p "$FreeRTOS_DST_Folder"
-fi
+mkdir -p "$FreeRTOS_DST_Folder"
 
-echo "Home : " $HOME
+echo "Home: $HOME"
 
-# Copy the contents from mbedTLS_source to mbedTLS_destination
-FILE_TO_COPY="FreeRTOSConfig.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder/examples/template_configuration"
-DST_FOLDER="$FreeRTOS_DST_Folder/Config/ARMCM/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
+# Function to copy files and echo progress
+copy_file() {
+    local src="$1"
+    local dst="$2"
+    cp "$src" "$dst"
+    echo "Copied $(basename "$src") from $src to $dst"
+}
 
-FILE_TO_COPY="croutine.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
+# List of files to copy
+files_to_copy=(
+    "examples/template_configuration/FreeRTOSConfig.h:Config/ARMCM/"
+    "croutine.c:Source/"
+    "list.c:Source/"
+    "queue.c:Source/"
+    "stream_buffer.c:Source/"
+    "tasks.c:Source/"
+    "timers.c:Source/"
+    "event_groups.c:Source/"
+    "portable/MemMang/heap_4.c:Source/portable/MemMang/"
+    "include/CMakeLists.txt:Source/include"
+    "include/FreeRTOS.h:Source/include"
+    "include/StackMacros.h:Source/include"
+    "include/atomic.h:Source/include"
+    "include/croutine.h:Source/include"
+    "include/deprecated_definitions.h:Source/include"
+    "include/event_groups.h:Source/include"
+    "include/message_buffer.h:Source/include"
+    "include/mpu_prototypes.h:Source/include"
+    "include/mpu_syscall_numbers.h:Source/include"
+    "include/mpu_wrappers.h:Source/include"
+    "include/newlib-freertos.h:Source/include"
+    "include/picolibc-freertos.h:Source/include"
+    "include/portable.h:Source/include"
+    "include/projdefs.h:Source/include"
+    "include/queue.h:Source/include"
+    "include/semphr.h:Source/include"
+    "include/stack_macros.h:Source/include"
+    "include/stdint.readme:Source/include"
+    "include/stream_buffer.h:Source/include"
+    "include/task.h:Source/include"
+    "include/timers.h:Source/include"
+    "include/list.h:Source/include"
+)
 
-FILE_TO_COPY="list.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
+# Copy each file
+for entry in "${files_to_copy[@]}"; do
+    IFS=":" read -r src_path dst_path <<< "$entry"
+    copy_file "$FreeRTOS_SRC_Folder/$src_path" "$FreeRTOS_DST_Folder/$dst_path"
+done
 
-FILE_TO_COPY="queue.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
+# Remove old portable directory and copy the new one
+rm -rf "$FreeRTOS_DST_Folder/Source/portable/$COMPILER/ARM_CM33_NTZ"
+copy_file "$FreeRTOS_SRC_Folder/portable/$COMPILER/$CORE" "$FreeRTOS_DST_Folder/Source/portable/$COMPILER/"
 
-FILE_TO_COPY="stream_buffer.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
+# Copy port.c specifically to a different destination
+copy_file "$FreeRTOS_SRC_Folder/portable/$COMPILER/$CORE/non_secure/port.c" "Middlewares/FreeRTOS/RTOS/Core/Cortex-M/"
 
-FILE_TO_COPY="tasks.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="timers.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="event_groups.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="heap_4.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder/portable/MemMang"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/portable/MemMang/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/CMakeLists.txt"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/FreeRTOS.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/StackMacros.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/atomic.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/croutine.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/deprecated_definitions.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/event_groups.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/message_buffer.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/mpu_prototypes.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/mpu_syscall_numbers.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/mpu_wrappers.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/newlib-freertos.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/picolibc-freertos.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/portable.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/projdefs.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/queue.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/semphr.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/stack_macros.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/stdint.readme"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/stream_buffer.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/task.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/timers.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="include/list.h"
-SRC_FOLDER="$FreeRTOS_SRC_Folder"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/include"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
-
-rm -rf "$FreeRTOS_DST_Folder/Source/portable/GCC/ARM_CM33_NTZ"
-
-SRC_FOLDER="$FreeRTOS_SRC_Folder/portable/GCC/ARM_CM55_NTZ"
-DST_FOLDER="$FreeRTOS_DST_Folder/Source/portable/GCC/"
-cp -r "$SRC_FOLDER" "$DST_FOLDER"
-echo "Content copied from $SRC_FOLDER to $DST_FOLDER"
-
-FILE_TO_COPY="port.c"
-SRC_FOLDER="$FreeRTOS_SRC_Folder/portable/GCC/ARM_CM55_NTZ/non_secure"
-DST_FOLDER="Middlewares/FreeRTOS/RTOS/Core/Cortex-M/"
-cp "$SRC_FOLDER/$FILE_TO_COPY" "$DST_FOLDER"
-echo "Copied $FILE_TO_COPY from $SRC_FOLDER to $DST_FOLDER"
+echo "All files copied successfully."
